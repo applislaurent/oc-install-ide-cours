47cf1fb926797b54be4e1ed696c2c0dcc0f603ff
applislaurent <l.perroy@wanadoo.fr>
2021-01-13 10:22:10 +0100

    modif depuis github

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int inde2;
+		int index2;
 		for(index2=0; index2>=10; index2++) {
 			System.out.println("Hello bugs !" + index2);
 		}
