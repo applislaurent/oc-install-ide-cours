6db2318a5060ad78dab855c9cbb1d88bc19e7df7
frup74672 <frup74672@GFI530515.annuaire.groupe.local>
2021-01-13 12:21:08 +0100

    modif méthode nouvelle classe

--- src/main/java/com/openclassrooms/ideinstall/MaNouvelleClasse.java
+++ src/main/java/com/openclassrooms/ideinstall/MaNouvelleClasse.java
@@ -5,6 +5,7 @@ public class MaNouvelleClasse {
 	
 	public static void blabla() {
 		System.out.println("ajout d'une méthode");
+		System.out.println("ajouté encore une ligné dans méthode");
 	}
 	
 	
