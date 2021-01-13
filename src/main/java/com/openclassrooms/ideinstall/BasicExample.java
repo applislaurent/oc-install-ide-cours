c71426b0da3edaf222a66dd73e56270e7ce54285
applislaurent <l.perroy@wanadoo.fr>
2021-01-13 10:13:29 +0100

    modif index1 -> index2

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,9 +3,9 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int index1;
-		for(index1=0; index1>=10; index1++) {
-			System.out.println("Hello bugs !" + index1);
+		int inde2;
+		for(index2=0; index2>=10; index2++) {
+			System.out.println("Hello bugs !" + index2);
 		}
 	}
 }
