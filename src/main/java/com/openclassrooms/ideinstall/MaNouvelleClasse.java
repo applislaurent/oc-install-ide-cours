64ce86329376b2e20a65c5782a3ff3bf6665dc05
frup74672 <frup74672@GFI530515.annuaire.groupe.local>
2021-01-13 16:50:09 +0100

    rvservse

--- src/main/java/com/openclassrooms/ideinstall/MaNouvelleClasse.java
+++ src/main/java/com/openclassrooms/ideinstall/MaNouvelleClasse.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class MaNouvelleClasse {
 
 	public static void blabla() {
-		System.out.println("modif depuis Github");
+		System.out.println("modif depuis local");
 	}
 	
 }
