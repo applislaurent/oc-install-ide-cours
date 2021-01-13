ad9109d455f665bd5a08629211c419062d433693
applislaurent <l.perroy@wanadoo.fr>
2021-01-13 16:39:22 +0100

    Modif depuis Github

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,8 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int indexA;
 		for(indexA=0; indexA>=10; indexA++) {
-			System.out.println("ligne initiale");
-			System.out.println("nouvelle ligne depuis Github");
+			System.out.println("modif depuis Github");
 		}
 	}
 }
