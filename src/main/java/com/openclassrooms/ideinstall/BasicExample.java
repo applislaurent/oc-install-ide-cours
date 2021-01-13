74c3003862a709736677d404ebb06f07f60f6aef
frup74672 <frup74672@GFI530515.annuaire.groupe.local>
2021-01-13 13:30:38 +0100

    reset

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,8 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int indexA;
 		for(indexA=0; indexA>=10; indexA++) {
-			System.out.println("Hello bugs !" + indexA);
-			System.out.println("et si");
+			System.out.println("ligne initiale");
 		}
 	}
 }
