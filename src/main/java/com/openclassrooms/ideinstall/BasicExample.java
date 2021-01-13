00b5019dd644743678ceef2631e51d068a09e61a
applislaurent <l.perroy@wanadoo.fr>
2021-01-13 09:46:31 +0100

    Nouveau changement nom variable, depuis github

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,9 +3,9 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int index1;
-		for(index1=0; index1>=10; index1++) {
-			System.out.println("Hello bugs !" + index1);
+		int indexToutCourt;
+		for(indexToutCourt=0; indexToutCourt>=10; indexToutCourt++) {
+			System.out.println("Hello bugs !" + indexToutCourt);
 		}
 	}
 }
